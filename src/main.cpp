0b7926c136bd17131422e33b7104fb0673189467
MattOlczyk <108824598+mattolczyk@users.noreply.github.com>
2026-08-18 05:00:07 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include "XYController.h"
+
+
+// GLOBAL INSTANCE - MUST HAVE !!!!
+XYController xy;
+
+void setup()
+{
+    Serial.begin(115200);
+    xy.begin();
+    xy.home();
+    Serial.println("XY KINETICS READY");
+}
+
+void loop()
+{
+//DEMO
+    xy.moveXYAndWait(100, 50);
+    delay(1000);
+    xy.moveXYAndWait(50, 100);
+    delay(1000);
+    xy.moveXYAndWait(0, 0);
+    Serial.printf(
+        "X=%.2f mm  Y=%.2f mm\n",
+        xy.getX(),
+        xy.getY());
+    xy.disableMotors();
+    delay(5000);
+//DEMO
+}
